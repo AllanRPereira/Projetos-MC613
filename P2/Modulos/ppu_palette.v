2bf59152031f06e75d7247a677a5b103ba4de461
AllanRPereira <allanflyk2@gmail.com>
2026-04-15 17:07:30 -0300

    Ajustes código

--- P2/Modulos/ppu_palette.v
+++ P2/Modulos/ppu_palette.v
@@ -6,21 +6,21 @@ module ppu_palette (
 	output wire [7:0] blue 
 	);
 	
-	wire [3:0] disp_color_idx;
-	wire [23:0] rgb;
-	reg [23:0] storage [15:0];
+wire [3:0] disp_color_idx;
+wire [23:0] rgb;
+reg [23:0] storage [15:0];
+
+assign disp_color_idx = (sprite_color_idx > 0) ? sprite_color_idx : bg_color_idx;
+
+initial begin
+$readmemh("palette.mem", storage);
+end
+
+assign rgb = storage[disp_color_idx];
+
+assign red = rgb[23:16];
+assign green = rgb[15:8];
+assign blue = rgb[7:0];
 	
-	assign disp_color_idx = (sprite_color_idx > 0) ? sprite_color_idx : bg_color_idx;
-	
-	initial begin
-    $readmemh("palette.mem", storage);
-	end
-	
-	assign rgb = storage[disp_color_idx];
-	
-	assign red = rgd[23:16];
-	assign green = rgb[15:8];
-	assign blue = rgb[7:0];
-	
-	endmodule
+endmodule
 	
--- P2/Modulos/ram_oam.v
+++ P2/Modulos/ram_oam.v
@@ -2,7 +2,7 @@ module ram_oam (
     input wire clk,
 
     // Coordenada do Tile na tela (Tiles: 32x32)
-    input wire [4:0] x_tite,
+    input wire [4:0] x_tile,
     input wire [4:0] y_tile,
 
     // Inputs para escrita
@@ -15,8 +15,8 @@ module ram_oam (
 );
 
 // OAM Memory
-// [X Ori] [Y Ori] [ID_SPRITE]
-//    5b      5b       5b
+// [ID_SPRITE] [X Ori] [Y Ori] 
+//    5b         5b       5b
 reg [14:0] sprite_mem [0:299];       // Possíveis 300 Sprites na tela
 
 // Inicialização da Memória
@@ -27,14 +27,16 @@ initial begin
 end
 
 wire [8:0] sprite_addr;
-assign sprite_addr = y_tile << 4 + y_tile << 2 + x_tite; // y_tile * 20 + x_tile
+assign sprite_addr = (y_tile << 4) + (y_tile << 2) + x_tile; // y_tile * 20 + x_tile
 
 
 always @(posedge clk) begin
     if (sig_write_ram) begin
-        sprite_mem[ram_addr] <= data_in;
+        sprite_mem[ram_addr] <= {sprite_mem[ram_addr][14:5], sprite_in};
     end
 end
 
 // Aqui apenas fornecemos os 5 últimos bits para o id_sprite:
 assign id_sprite = sprite_mem[sprite_addr][4:0];
+
+endmodule
