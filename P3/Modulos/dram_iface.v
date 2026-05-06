ea9a13b28389eedf43bae6ffe019a0d471f87dd4
AllanRPereira <allanflyk2@gmail.com>
2026-05-06 14:20:56 -0300

    Ajustes dram_iface

--- P3/Modulos/dram_iface.v
+++ P3/Modulos/dram_iface.v
@@ -4,11 +4,11 @@ module dram_iface (
     input wire [9:0] SW,
     input wire [3:0] KEY,
     input wire ready,
+    input wire [7:0] data,
 
     output wire [6:0] HEX0, HEX1, HEX4, HEX5,
 
     output reg [25:0] address,
-    output reg [7:0] data,
     output reg req,
     output reg wEn
 );
@@ -26,8 +26,8 @@ module dram_iface (
     reg [3:0] dado_exibir = 4'b0000;
     reg [3:0] dado_escrito = 4'b0000;
 
-    assign HEX1 = dado_exibir;
-    assign HEX0 = dado_escrito;
+    assign HEX1 = {3'b000, dado_exibir};
+    assign HEX0 = {3'b000, dado_escrito};
 
     assign HEX4 = {3'b000, address[22:21], address[1:0]};       // Parte baixa da área endereçável
     assign HEX5 = {5'b00000, address[25], address[23]};         // Parte alta da área endereçável
@@ -35,12 +35,8 @@ module dram_iface (
     always @(posedge clk) begin 
         if (rst) begin
             estado <= READY;
-            address <= 26'b00000000000000000000000000;
-            data <= 8'b00000000;
             dado_escrito <= 4'b0000;
             dado_exibir <= 4'b0000;
-            wEn <= 1'b0;
-            req <= 1'b0;
         end else begin 
             case (estado)
                 READY: begin
@@ -65,72 +61,86 @@ module dram_iface (
 
                 WAIT_WRITE: begin
                     if (ready) begin
+                        dado_escrito <= data[3:0];
                         estado <= REQ_READ;
                     end
                 end
 
                 WAIT_READ: begin
                     if (ready) begin
+                        dado_exibir <= data[3:0];
                         estado <= READY;
                     end
                 end
 
-                default: estado <= estado;
+                default: begin 
+                    estado <= estado;
+                    dado_escrito <= 4'b0000;
+                    dado_exibir <= 4'b0000;
+                end
             endcase
         end
     end
 
-    always @(*) begin
-    
-        case (estado_atual) begin
-            READY: begin
-                address <= 26'b00000000000000000000000000;
-                data <= 8'b00000000;
-                wEn <= 1'b0;
-                req <= 1'b0;
-            end
+    always @(posedge clk) begin
+        if (rst) begin
+            address <= 26'b00000000000000000000000000;
+            endereco_ultima_leitura <= 6'b000000;
+            data <= 8'b00000000;
+            wEn <= 1'b0;
+            req <= 1'b0;
+        end else begin 
+            case (estado)
+                READY: begin
+                    address <= 26'b00000000000000000000000000;
+                    data <= 8'b00000000;
+                    wEn <= 1'b0;
+                    req <= 1'b0;
+                end
 
-            REQ_READ: begin
-                address <= 26'b00000000000000000000000000;
-                address[25] <= SW[9];
-                address[23:21] <= SW[8:6];
-                address[1:0] <= SW[5:4];
-                endereco_ultima_leitura <= SW[9:4];
-                data <= 8'b00000000;
-                req <= 1'b1;
-            end
+                REQ_READ: begin
+                    address <= 26'b00000000000000000000000000;
+                    address[25] <= SW[9];
+                    address[23:21] <= SW[8:6];
+                    address[1:0] <= SW[5:4];
+                    endereco_ultima_leitura <= SW[9:4];
+                    data <= 8'b00000000;
+                    wEn <= 1'b0;
+                    req <= 1'b1;
+                end
 
-            WAIT_READ: begin
-                dado_exibir <= data[3:0];
-            end
+                WAIT_READ: begin
+                    wEn <= 1'b0;
+                    req <= 1'b0;
+                end
 
-            REQ_WRITE: begin
-                address <= 26'b00000000000000000000000000;
-                address[25] <= SW[9];
-                address[23:21] <= SW[8:6];
-                address[1:0] <= SW[5:4];
-                data[7:4] <= 4'b0000;
-                data[3:0] <= SW[3:0];
-                wEn <= 1'b1;
-                req <= 1'b1;
+                REQ_WRITE: begin
+                    address <= 26'b00000000000000000000000000;
+                    address[25] <= SW[9];
+                    address[23:21] <= SW[8:6];
+                    address[1:0] <= SW[5:4];
+                    data[7:4] <= 4'b0000;
+                    data[3:0] <= SW[3:0];
+                    wEn <= 1'b1;
+                    req <= 1'b1;
 
-            end
+                end
 
-            WAIT_WRITE: begin
-                dado_escrito <= data[3:0];
-            end
+                WAIT_WRITE: begin
+                    wEn <= 1'b0;
+                    req <= 1'b0;
+                end
 
-            default: begin
-                address <= 26'b00000000000000000000000000;
-                data <= 8'b00000000;
-                dado_escrito <= 4'b0000;
-                dado_exibir <= 4'b0000;
-                wEn <= 1'b0;
-                req <= 1'b0;
-            end
+                default: begin
+                    address <= 26'b00000000000000000000000000;
+                    endereco_ultima_leitura <= SW[9:4];
+                    data <= 8'b00000000;
+                    wEn <= 1'b0;
+                    req <= 1'b0;
+                end
 
-        endcase
-
-    end
+            endcase
+        end
+    end 
 
 endmodule
